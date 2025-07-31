edaa8d67f5df70a5a70459e6c1804a9eb0979a47
rhaihanadzani <mrhaihan17@gmail.com>
2025-07-31 23:30:39 +0700

    huhuhu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,14 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', '') }}</title>
+    <meta name="author" content="Atafia" />
+    <meta name="description"
+        content="We are registered as a trading company for Steam Coal and Nickel ore by Indonesian Government - ESDM Jakarta - Indonesia." />
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
     {{-- Favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/revi1-apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/revi1-favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/revi1-favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/revi2-apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/revi2-favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/revi2-favicon-16x16.png">
     <link rel="manifest" href="/favicons/site.webmanifest">
 
     <!-- Scripts -->
